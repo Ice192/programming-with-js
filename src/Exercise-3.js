0af46a9d015cd47840377617fa1e1f9d80093a4a
muhammadraisb2@gmail.com <muhammadraisb2@gmail.com>
2026-04-01 09:06:30 +0800

    Exercise 3 done

---
+++ src/Exercise-3.js
@@ -0,0 +1,7 @@
+function Convert (c) {
+    return 9 / 5 * c + 32
+}
+
+module.exports = {
+    Convert
+}
--- src/index.js
+++ src/index.js
@@ -13,14 +13,18 @@ console.log("Force = mass * acceleration");
 console.log("Force with mass=10kg and acceleration=9.8m/s^2 =", force(10, 9.8));
 console.log("---------------------------------------------------------")
 
-const {nama, address, telp, major } = require("./Exercise1.js")
+const {nama, address, telp, major } = require("./Exercise-1.js")
 nama("Rais");
 address("Daya", "Makassar", "South Sulawesi", "19954")
 telp("0865-8464-2745")
 major("Software Engineering")
 console.log("---------------------------------------------------------")
 
-const {Distance} = require ("./Exercise2.js")
+const {Distance} = require ("./Exercise-2.js")
 console.log("The distance the car will travel in 6 hours is", Distance(6), "miles")
 console.log("The distance the car will travel in 10 hours is", Distance(10), "miles")
 console.log("The distance the car will travel in 15 hours is", Distance(15), "miles")
+console.log("---------------------------------------------------------")
+
+const { Convert } = require ("./Exercise-3.js")
+console.log("10 F =", Convert(10), "C")
