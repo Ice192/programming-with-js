6a75adf8b9af21f05b865c30ff1539f79180358e
muhammadraisb2@gmail.com <muhammadraisb2@gmail.com>
2026-04-08 14:56:00 +0800

    update nested loop

--- src/nested-loop.js
+++ src/nested-loop.js
@@ -1,25 +1,30 @@
 function FirstNested ()  {
     for (let i = 1; i <= 7; i++) {
+        let line = ""
         for (let j = 7; j > i; j--) {
-            process.stdout.write(`* `)
+            line += "*" + ""
     }
-    console.log()
+    console.log(line)
 }
 }
 
 FirstNested()
 
 function SecondNested ()  {
-    for (let i = 3; i < 9; i++) {
-        for (let j = 1; i > j; j++) {
-            if(i === j) {
-                process.stdout.write(` `)
-            } else {
-                process.stdout.write(`#`)
-            }
+    let line = ""
+    for (let i = 0 ; i < 7; i++) {
+        line += "#"
+        for (let j = 0; j < i; j++) {
+            line += " "
+            // if(i === j) {
+            //     line += ""
+            // } else {
+            //     line += "#"
+            // }
     }
-    console.log()
+    line += "#\n"
 }
+console.log(line)
 }
 
 SecondNested()
