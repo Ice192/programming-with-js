72bcb0e5a85f644fea75dfce09bcfeb038586326
muhammadraisb2@gmail.com <muhammadraisb2@gmail.com>
2026-04-14 13:49:53 +0800

    update

---
+++ src/files.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+
+
+// fs.readFile('./files/starter.txt','utf8', (err, data) => {
+//     if(err) throw err
+//     console.log(data) // tambahkan parameter utf8 kalau tidak ingin menggunakan method toString()
+// })
+
+// fs.writeFile ('./files/reply.txt', 'Nice to meet you too', (err) => {
+//     if(err) throw err
+//     console.log("Write Complete")
+// })
+
+// fs.appendFile('./files/replya.txt', '\nNice to meet you', (err) => {
+//     if(err) throw err
+//     console.log("Update Complete")
+// }) // append untuk update file dan bisa membuat file baru
+
+
+function randomNumber(number) {
+    const hasil = []
+    for (let i = 0; i < number; i++){
+        const number = Math.floor(Math.random()*500) + 1
+        hasil.push(number)
+    }
+    return hasil
+}
+
+const hasil = randomNumber(20)
+
+fs.writeFile('./files/reply.txt', hasil.join(), (err) => {
+    if(err) throw err
+    console.log("Write Complete")
+})
