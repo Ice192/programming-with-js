89b3822d25e0766461544d21055ea46bd8e03aa2
muhammadraisb2@gmail.com <muhammadraisb2@gmail.com>
2026-04-14 08:56:48 +0800

    update

--- src/loop-string.js
+++ src/loop-string.js
@@ -25,5 +25,29 @@ let kata = ""
     return jumlahKata
 }
 
+const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+function cekAlphabet (alphabet, kalimat) {
+    const kumpulanHuruf = []
+    for (const x of alphabet){
+        let find = false
+        for(const y of kalimat){
+            if(x === y) {
+                find = true
+                break
+            }
+        }
+        if (!find) {
+            kumpulanHuruf.push(x)
+        }
+
+    }
+
+    return kumpulanHuruf
+
+    
+}
 
 console.log(hitungKata(kalimatFavoritKu)) //20
+
+console.log(cekAlphabet(alphabet, kalimatFavoritKu))
