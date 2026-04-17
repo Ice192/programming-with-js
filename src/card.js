8098ac15b5cfa6f5cd8db195c83c1bd9d56233e9
muhammadraisb2@gmail.com <muhammadraisb2@gmail.com>
2026-04-17 09:47:13 +0800

    update card

--- src/card.js
+++ src/card.js
@@ -1,5 +1,5 @@
 const simbol = ['♠️', '❤️', '♦️', '♣️']
-const angka = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+const angka = [ '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
 function setCard (simbol, angka) {
     const card = []
@@ -34,5 +34,117 @@ function totalShuffle(arr, iteration){
     return arr
 }
 
-const random_shuffle = Math.floor(Math.random() * 100) + 1
-console.log(totalShuffle(card, random_shuffle))
+// const random_shuffle = Math.floor(Math.random() * 100) + 1
+// console.log(totalShuffle(card, random_shuffle))
+
+
+function reshuffle(arr) {
+    const result = []
+    for (let i = 0; i < arr.length; i++){
+        const index = Math.floor(Math.random()*arr.length - 1)
+        
+        result.push(arr[index])
+    }
+    return result
+}
+
+// console.log(reshuffle(card))
+
+function thirdShuffle(arr) {
+    for (let i = 0; i < 1000; i++){
+        const a = Math.floor(Math.random() * (arr.length - 1))
+        const b = Math.floor(Math.random() * (arr.length - 1))
+
+        if (a === b){
+            continue
+        }
+
+        const temp = arr[a]
+        arr[a] = arr[b]
+        arr[b] = temp
+    }
+    return arr
+}
+
+const final_card = thirdShuffle(card)
+
+function player (card, total){
+    const result = []
+    for (let i = 0; i < total; i++){
+        const pick = Math.floor(Math.random() * (card.length - 1))
+
+        const take = card.splice(pick,1)
+        result.push(take[0])
+    }
+
+    return result
+}
+
+const you = player(final_card, 2)
+const enemy = player(final_card, 2)
+const bandar = player(final_card, 5)
+console.log(you)
+console.log(enemy)
+console.log(bandar)
+
+console.log(final_card)
+
+
+function cekPair (card, bandar){
+    const result = []
+    const temp = card.concat(bandar)
+    let count_pair = 0
+   
+    for (let i = 0; i < temp.length;i++){
+        const aCard = temp[i].split("  ")
+        const angka = aCard[1]
+        result.push(angka)    
+    }
+    const pair_used = []
+    for (let i = 0; i < result.length - 1; i++){
+        if(pair_used.includes(result[i])){
+            continue
+        }
+        for (let j = i + 1; j < result.length; j++){
+        if(pair_used.includes(result[j])){
+            continue
+        }
+            
+            if (result[i] === result[j]){
+                console.log(`${result[i]} = ${result[j]}`)
+                count_pair += 1
+                pair_used.push(result[i])
+            }
+        }
+    }
+
+    (count_pair > 2) ? count_pair = 2 : count_pair
+
+    return count_pair
+}
+
+ const final_you = cekPair(you, bandar)
+ if (final_you === 0) {
+    console.log("You don't have a pair")
+ } else {
+    console.log(`You have ${final_you} pair`)
+ }
+ const final_enemy = cekPair(enemy, bandar)
+  if (final_enemy === 0) {
+    console.log("Enemy don't have a pair")
+ } else {
+    console.log(`Enemy have ${final_enemy} pair`)
+ }
+
+
+function whoWin (player, enemy){
+    if (player === enemy){
+        return "Draw"
+    } else if (player > enemy) {
+        return "You Win"
+    } else {
+        return "Enemy Win"
+    }
+}
+
+console.log(whoWin(final_you,final_enemy))
