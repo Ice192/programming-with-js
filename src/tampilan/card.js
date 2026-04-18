ae22b6aec46ce40e65c2454c4104bbb9c1a47990
muhammadraisb2@gmail.com <muhammadraisb2@gmail.com>
2026-04-18 08:36:48 +0800

    update card

--- src/tampilan/card.js
+++ src/tampilan/card.js
@@ -1,6 +1,7 @@
 const simbol = ['♠️', '❤️', '♦️', '♣️']
 const angka = [ '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
+
 function setCard (simbol, angka) {
     const card = []
     for (const x of angka){
@@ -83,22 +84,39 @@ function player (card, total){
 const you = player(final_card, 2)
 const enemy = player(final_card, 2)
 const bandar = player(final_card, 5)
-console.log(you)
-console.log(enemy)
+console.log("you",you)
+console.log("Enemy",enemy)
 console.log(bandar)
 
 console.log(final_card)
 
+function getAngka (card){
+    const angka = card.split("  ")
+    return angka[1]
+}
+
+function getSymbol (card) {
+    const symbol = card.split("  ")
+    return symbol[0]
+}
+
+function getColor (symbol) {
+    let color = "black"
+    if (symbol === "❤️" || symbol === "♦️"){
+        color = "red"
+    }
+
+    return color
+}
 
 function cekPair (card, bandar){
     const result = []
     const temp = card.concat(bandar)
     let count_pair = 0
    
-    for (let i = 0; i < temp.length;i++){
-        const aCard = temp[i].split("  ")
-        const angka = aCard[1]
-        result.push(angka)    
+    for (const card of temp){
+        const aCard = getAngka(card)
+        result.push(aCard)    
     }
     const pair_used = []
     for (let i = 0; i < result.length - 1; i++){
@@ -147,3 +165,50 @@ function whoWin (player, enemy){
 }
 
 console.log(whoWin(final_you,final_enemy))
+
+
+// UI function
+function getCardUI (card){
+    const symbol = getSymbol(card)
+    const color = getColor(symbol)
+
+    const myElement = document.createElement("div")
+    myElement.classList.add("card", color)
+    myElement.innerHTML = card
+
+    return myElement
+}
+
+const button_play = document.querySelector('#play')
+
+button_play.addEventListener('click', () => {
+    const playerContainer = document.querySelector('#player')
+    const enemyContainer = document.querySelector('#enemy')
+    const dealerContainer = document.querySelector('#dealer')
+
+    // player
+    for (const x of you) {
+        const el = getCardUI(x)
+        playerContainer.append(el)
+    }
+
+    // enemy
+    for (const x of enemy) {
+        const el = getCardUI(x)
+        enemyContainer.append(el)
+    }
+
+    // dealer
+    for (const x of bandar) {
+        const el = getCardUI(x)
+        dealerContainer.append(el)
+    }
+})
+
+// document.addEventListener('DOMContentLoaded', (ev) => {
+//     const player = document.querySelector('#player')
+//     for(const x of you){
+//         const myElement = getCardUI(x)
+//         player.append(myElement)
+//     }
+// })
