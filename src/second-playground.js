593896bd5dcc64e8b2e04e740f0c87415f72a79b
muhammadraisb2@gmail.com <muhammadraisb2@gmail.com>
2026-04-24 09:39:39 +0800

    update blackjack UI

--- src/second-playground.js
+++ src/second-playground.js
@@ -2,6 +2,7 @@ class Card {
     constructor(number, suit){
         this.suit = suit
         this.number = number
+        this.show = true
     }
 
     isOdd() {
@@ -24,44 +25,52 @@ class Card {
         }
         return isRed
     }
-}
 
-const symbols = ['♠️', '❤️', '♦️', '♣️']
-const numbers = [ 'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-const myDeck = []
-
-for(const number of numbers){
-    for(const symbol of symbols){
-        const card = new Card (number,symbol)
-        myDeck.push(card)
+    isShow(){
+        if(this.show){
+            return `${this.suit}  of ${this.number}`
+        } else {
+            return "####"
+        }
     }
 }
 
-console.log(myDeck)
+class Deck {
+    constructor(){
+        this.cards = []
+        this.seed()
+    }
 
-// for(const item of myDeck){
-//     if (item.isOdd()){
-//         if(item.isRed()){
-//             console.log(`Kartu ${item.number} ${item.suit}`)
-//         }
-//     }
-// }
+    seed(){
+        const symbols = ['♠️', '❤️', '♦️', '♣️']
+        const numbers = [ 'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        for(const number of numbers){
+            for(const symbol of symbols){
+                const card = new Card (number,symbol)
+                this.cards.push(card)
+                }
+        }
+    }
 
-function shuffle (arr) {
-    for (let i = 0; i < myDeck.length; i++) {
-                const j = Math.floor(Math.random() * myDeck.length);
+    shuffle () {
+    for (let i = 0; i < this.cards.length; i++) {
+            const j = Math.floor(Math.random() * this.cards.length);
             if (i === j){
                 continue
             }
-        const temp = myDeck[i]
-        myDeck[i] = myDeck[j]
-        myDeck[j] = temp
+        const temp = this.cards[i]
+        this.cards[i] = this.cards[j]
+        this.cards[j] = temp
         }
+        // return this.myDeck
 }
-shuffle(myDeck)
 
-console.log(myDeck)
+    draw(amount) {
+        return this.cards.splice(0,amount)
+    }
+}
 
-
-
-
+const myCard = new Deck()
+console.log(myCard.cards.length)
+console.log(myCard.draw(2))
+console.log(myCard.cards.length)
---
+++ src/tampilan2/card-in-class.js
@@ -0,0 +1,167 @@
+const symbols = ['♠️', '❤️', '♦️', '♣️']
+const numbers = [ 'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+class Card {
+    constructor(number, suit){
+        this.suit = suit
+        this.number = number
+        this.show = true
+        this.isRed()
+    }
+
+    isOdd() {
+        {
+        let isOdd
+        if(this.number === "Joker"){
+            return isOdd
+        }
+        if(this.number % 2 === 0 || this.number === "Q"){
+            isOdd = false}
+            else
+            {isOdd = true}      
+        return isOdd
+    } 
+    }
+    isRed(){
+        let isRed = false
+        if(this.suit === "❤️" || this.suit === "♦️" || this.suit === "Joker"){
+            isRed = true
+        }
+        return isRed
+    }
+
+    isShow(){
+        if(this.show){
+            return `${this.suit}${this.number}`
+        } else {
+            return "??"
+        }
+    }
+}
+
+class Deck {
+    constructor(){
+        this.cards = []
+        this.seed()
+    }
+
+    seed(){
+        for(const number of numbers){
+            for(const symbol of symbols){
+                const card = new Card (number,symbol)
+                this.cards.push(card)
+                }
+        }
+    }
+
+    shuffle () {
+    for (let i = 0; i < this.cards.length; i++) {
+            const j = Math.floor(Math.random() * this.cards.length);
+            if (i === j){
+                continue
+            }
+        const temp = this.cards[i]
+        this.cards[i] = this.cards[j]
+        this.cards[j] = temp
+        }
+        // return this.myDeck
+}
+
+    draw(amount) {
+        return this.cards.splice(0,amount)
+    }
+
+    level(){
+    for(const card of this.cards){
+        if (card.number === 'A'){
+            card.number = 11
+        } else if(card.number === 'J' || card.number === 'Q' || card.number === 'K'){
+            card.number = 10
+        } else {
+           card.number = Number(card.number)
+        }
+    }
+    return this.cards
+}
+}
+
+function createCard (number, suit) {
+    
+    try{
+        if(numbers.includes(number) && symbols.includes(suit)){
+            const card = new Card (number,suit)
+            return card
+        } else {
+            throw new Error("Anda memasukkan angka atau suit yang salah")
+        }
+    }catch(err){
+        console.log(err)
+    }
+}
+const cards = new Deck()
+console.log(createCard('14','❤️'))
+
+// UI function
+function setCardUI (cards) {
+    const myElement = document.createElement('div')
+    myElement.classList.add("card")
+    myElement.innerHTML = cards
+
+    return myElement
+}
+function setButtonDrawUI (){
+    const myElement = document.createElement('button')
+    myElement.setAttribute('id','draw')
+    myElement.innerHTML = 'Draw'
+
+    myElement.addEventListener('click', () => {
+    const player = document.querySelector('#player')
+    const playerCard = cards.draw(1)
+    for(const x of playerCard){
+        const result = x.isShow()
+        player.append(setCardUI(result))
+    }
+})
+
+    return myElement
+}
+
+
+const start = document.querySelector('#start')
+
+start.addEventListener('click', () => {
+    const deck = document.querySelector('#deck')
+    deck.innerHTML = ''
+    cards.shuffle()
+
+    const player = document.querySelector('#player')
+    const enemy = document.querySelector('#enemy')
+
+
+    player.innerHTML = ''
+    enemy.innerHTML = ''
+
+    const playerCard = cards.draw(2)
+    const enemyCard = cards.draw(2)
+    player.append(setButtonDrawUI())
+    
+    for(const x of playerCard){
+        const result = x.isShow()
+        player.append(setCardUI(result))
+    }
+
+    for(const x of enemyCard){
+        x.show = false
+        const result = x.isShow()
+        enemy.append(setCardUI(result))
+    } 
+    
+    const element = document.createElement('div')
+    element.classList.add("card")
+    element.innerHTML = cards.cards.length
+    deck.append(element)
+})
+
+
+
+
+
